18c701d749eab5647acb2675d85a6b4885abec3b
TROLLSTEFAN <trollstefan1@gmail.com>
2020-05-07 19:04:34 +0200

    form window

--- src/main/java/symulator/gui/FormController.java
+++ src/main/java/symulator/gui/FormController.java
@@ -1,4 +1,18 @@
 package symulator.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
+
 public class FormController {
+
+
+    @FXML
+    private ComboBox comboBox;
+
+
+    @FXML
+    public void initialize()
+    {
+        comboBox.getItems().add("Kredyt");
+    }
 }
