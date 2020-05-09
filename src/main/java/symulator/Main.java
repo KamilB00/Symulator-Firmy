f4818ad0ae73bf70f9a8ed4541b9e4e74abf58a6
KamilB00 <bonkowski-kamil@o2.pl>
2020-05-09 15:48:41 +0200

    few changes

--- src/main/java/symulator/Main.java
+++ src/main/java/symulator/Main.java
@@ -10,8 +10,8 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("/gui/Credit.fxml"));
-        primaryStage.setTitle("Hello World");
+        Parent root = FXMLLoader.load(getClass().getResource("/gui/Form.fxml"));
+        primaryStage.setTitle("Symulacja firmy");
         primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
--- src/main/java/symulator/gui/FormController.java
+++ src/main/java/symulator/gui/FormController.java
@@ -1,7 +1,16 @@
 package symulator.gui;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ComboBox;
+import javafx.stage.Stage;
+
+import javafx.event.ActionEvent;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 public class FormController {
 
@@ -17,7 +26,16 @@ public class FormController {
         comboboxForm1.getItems().add("Venture Capitals");
         comboboxForm1.getItems().add("Inwestor");
         comboboxForm1.getItems().add("Własne środki");
-    }
 
+    }
+    @FXML
+    private void addScene(ActionEvent event)throws IOException{
+        Parent view2 = FXMLLoader.load(getClass().getResource("/gui/VC.fxml"));
+        Scene scene2 = new Scene(view2);
+        Stage window = new Stage();
+        window.initStyle(StageStyle.UNDECORATED);
+        window.setScene(scene2);
+        window.show();
+    }
 
 }
