faf342de07ca4899aa456fc5c0b804d534690e52
andbob885 <andbob885@gmail.com>
2020-05-09 19:00:28 +0200

    Simulation_kontrolery

--- src/main/java/symulator/gui/SimulationController.java
+++ src/main/java/symulator/gui/SimulationController.java
@@ -1,4 +1,226 @@
 package symulator.gui;
 
+import javafx.fxml.FXML;
+import javafx.scene.chart.LineChart;
+import javafx.scene.chart.PieChart;
+import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
+import javafx.scene.control.TextField;
+
 public class SimulationController {
-}
+
+
+        @FXML
+        private TextField kTextField12;
+
+        @FXML
+        private TextField kTextField3;
+
+        @FXML
+        private TextField fTextField10;
+
+        @FXML
+        private ProgressBar progressBarSim0;
+
+        @FXML
+        private ProgressBar progressBarSim1;
+
+        @FXML
+        private ProgressBar progressBarSim2;
+
+        @FXML
+        private ProgressBar progressBarSim3;
+
+        @FXML
+        private ProgressBar progressBarSim4;
+
+        @FXML
+        private TextField fTextField0;
+
+        @FXML
+        private TextField bTextField0;
+
+        @FXML
+        private TextField sTextField0;
+
+        @FXML
+        private TextField kTextField0;
+
+        @FXML
+        private TextField gTextField0;
+
+        @FXML
+        private TextField fTextField1;
+
+        @FXML
+        private TextField fTextField2;
+
+        @FXML
+        private TextField fTextField3;
+
+        @FXML
+        private TextField fTextField4;
+
+        @FXML
+        private TextField fTextField6;
+
+        @FXML
+        private TextField fTextField7;
+
+        @FXML
+        private TextField fTextField8;
+
+        @FXML
+        private TextField fTextField9;
+
+        @FXML
+        private TextField fTextField14;
+
+        @FXML
+        private TextField fTextField13;
+
+        @FXML
+        private TextField fTextField12;
+
+        @FXML
+        private TextField fTextField11;
+
+        @FXML
+        private Label fLabel2;
+
+        @FXML
+        private Label fLabel1;
+
+        @FXML
+        private TextField bTextField1;
+
+        @FXML
+        private TextField bTextField2;
+
+        @FXML
+        private TextField bTextField3;
+
+        @FXML
+        private TextField bTextField4;
+
+        @FXML
+        private TextField bTextField5;
+
+        @FXML
+        private TextField bTextField6;
+
+        @FXML
+        private TextField bTextField7;
+
+        @FXML
+        private TextField bTextField8;
+
+        @FXML
+        private TextField bTextField9;
+
+        @FXML
+        private TextField bTextField11;
+
+        @FXML
+        private TextField bTextField12;
+
+        @FXML
+        private TextField bTextField13;
+
+        @FXML
+        private Label bLabel1;
+
+        @FXML
+        private Label bLabel3;
+
+        @FXML
+        private Label bLabel4;
+
+        @FXML
+        private Label bLabel5;
+
+        @FXML
+        private Label bLabel2;
+
+        @FXML
+        private TextField kTextField1;
+
+        @FXML
+        private PieChart sPieChart;
+
+        @FXML
+        private LineChart<?, ?> sLineChart;
+
+        @FXML
+        private LineChart<?, ?> gLineChart;
+
+        @FXML
+        private TextField kTextField2;
+
+        @FXML
+        private TextField kTextField5;
+
+        @FXML
+        private TextField kTextField6;
+
+        @FXML
+        private TextField kTextField7;
+
+        @FXML
+        private TextField kTextField8;
+
+        @FXML
+        private TextField kTextField9;
+
+        @FXML
+        private TextField kTextField16;
+
+        @FXML
+        private TextField kTextField17;
+
+        @FXML
+        private Label kLabel6;
+
+        @FXML
+        private Label kLabel7;
+
+        @FXML
+        private TextField kTextField18;
+
+        @FXML
+        private Label kLabel8;
+
+        @FXML
+        private TextField kTextField10;
+
+        @FXML
+        private TextField kTextField11;
+
+        @FXML
+        private Label kLabel1;
+
+        @FXML
+        private Label kLabel2;
+
+        @FXML
+        private Label kLabel3;
+
+        @FXML
+        private TextField kTextField13;
+
+        @FXML
+        private Label kLabel4;
+
+        @FXML
+        private TextField kTextField15;
+
+        @FXML
+        private Label kLabel5;
+
+        @FXML
+        private TextField kTextField4;
+
+        @FXML
+        private TextField fTextField5;
+
+    }
