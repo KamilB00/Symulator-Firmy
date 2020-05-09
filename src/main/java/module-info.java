bc915dca8f2e423dd40e40683b3f7f16cb3cf057
TROLLSTEFAN <trollstefan1@gmail.com>
2020-05-09 18:16:31 +0200

    Formcss

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,2 +1,8 @@
-module $MODULE_NAME$ {
+module symulator.firmy.v2 {
+    requires javafx.graphics;
+    requires javafx.fxml;
+    requires javafx.controls;
+    exports symulator to javafx.graphics;
+    opens symulator.gui to javafx.fxml;
+
 }
--- src/main/java/symulator/gui/FormController.java
+++ src/main/java/symulator/gui/FormController.java
@@ -2,13 +2,21 @@ package symulator.gui;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Button;
 
 public class FormController {
 
 
     @FXML
     private ComboBox comboboxForm1;
+    @FXML
+    private Button startSimulationButton;
 
+    @FXML
+    private Button presentationButton;
+
+    @FXML
+    private Button buttonForm1;
 
     @FXML
     public void initialize()
