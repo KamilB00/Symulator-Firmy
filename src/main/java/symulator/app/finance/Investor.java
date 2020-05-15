e930fa0a644acd3f83241eb3a9d92607a93fc6e6
matt1easy <matichalikmati@gmail.com>
2020-05-15 11:34:31 +0200

    funkcje randomowe

--- src/main/java/symulator/app/finance/Investor.java
+++ src/main/java/symulator/app/finance/Investor.java
@@ -1,13 +1,42 @@
 package symulator.app.finance;
+import java.util.Random;
 
 public class Investor {
-   private double amount;
-   private double yearProfit;
-   private double time;
+   private double amount = randomAmount();
+   private double yearProfit = randomYearProfit();
+   private double time = randomTime();
 
 
    //TODO MATEUSZ
    // 1. funkcjie Randomowe dla czasu w latach
    // 2. funcje randomowe dla kwoty oferowanej przez inwestora
    // 3. funkcje randomowe dla yearProfit w procentach
+
+
+   double randomTime() {
+      Random random = new Random();
+      return (double) (random.nextInt(10) + 1);   //od 1 do 10 lat
+   }
+
+   double randomAmount() {
+      Random random2 = new Random();
+
+      if(time == 1) return (double) (random2.nextInt(5) + 1) * 100000;
+      else if(time == 2) return (double) (random2.nextInt(11) + 5) * 100000;
+      else if(time == 3) return (double) (random2.nextInt(16) + 10) * 100000;
+      else if(time == 4) return (double) (random2.nextInt(21) + 15) * 100000;
+      else if(time == 5) return (double) (random2.nextInt(26) + 20) * 100000;
+      else if(time == 6) return (double) (random2.nextInt(31) + 25) * 100000;
+      else if(time == 7) return (double) (random2.nextInt(36) + 30) * 100000;
+      else if(time == 8) return (double) (random2.nextInt(41) + 35) * 100000;
+      else if(time == 9) return (double) (random2.nextInt(46) + 40) * 100000;
+      else if(time == 10) return (double) (random2.nextInt(51) + 45) * 100000;
+         else return 0;
+   }
+
+   double randomYearProfit() {
+      Random random3 = new Random();
+      return (double) random3.nextInt(101);
+   }
+
 }
--- src/main/java/symulator/app/finance/VC.java
+++ src/main/java/symulator/app/finance/VC.java
@@ -1,13 +1,23 @@
 package symulator.app.finance;
 
-
-
+import java.util.Random;
 
 public class VC {
-    private double amount;
-    private double shares;
+    private double amount = randomAmount();
+    private double shares = randomShares();
 
-//TODO MATEUSZ
+
+    //TODO MATEUSZ
     // 1. funkcje randomowe dla udziałów i oferowanej kwoty
+
+    double randomAmount() {
+        Random random2 = new Random();
+        return (double) (random2.nextInt(10) + 1) * 200000;  //od 200k do 2kk
+    }
+
+    double randomShares() {
+        Random random3 = new Random();
+        return (double) random3.nextInt(21) + 10;   //od 10% do 30%
+    }
 
 }
